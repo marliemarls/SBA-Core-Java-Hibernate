f6e5663e6b3d378f984d01647a557bc198f81008
Marlie Martinez <marlieahue@gmail.com>
2024-08-09 12:55:58 -0400

    first commit

---
+++ src/main/java/sba/sms/models/Course.java
@@ -0,0 +1,52 @@
+package sba.sms.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "course")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ToString
+
+
+/**
+ * Course is a POJO, configured as a persistent class that represents (or maps to) a table
+ * name 'course' in the database. A Course object contains fields that represent course
+ * information and a mapping of 'courses' that indicate an inverse or referencing side
+ * of the relationship. Implement Lombok annotations to eliminate boilerplate code.
+ */
+
+public class Course {
+    @Column(name = "ID")
+    protected int id;
+    @Column(name = "Name")
+    public String name;
+    @Column(name = "Instructor")
+    public String instructor;
+    @Column(name = "Students")
+    public Set<Student> students;
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && Objects.equals(name, course.name) && Objects.equals(instructor, course.instructor) && Objects.equals(students, course.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, instructor, students);
+    }
+}
---
+++ src/main/java/sba/sms/models/Student.java
@@ -0,0 +1,57 @@
+package sba.sms.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+/**
+ * Student is a POJO, configured as a persistent class that represents (or maps to) a table
+ * name 'student' in the database. A Student object contains fields that represent student
+ * login credentials and a join table containing a registered student's email and course(s)
+ * data. The Student class can be viewed as the owner of the bi-directional relationship.
+ * Implement Lombok annotations to eliminate boilerplate code.
+ */
+
+@Entity
+@Table(name = "student")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ToString
+
+
+public class Student {
+    @Id
+    @Column(name = "Name")
+    public String name;
+    @Column(name = "Email")
+    public String email;
+    @Column(name = "Password")
+    private String password;
+    @Column(name = "course")
+    public Set<Course> course;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(password, student.password) && Objects.equals(course, student.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, password, course);
+    }
+}
+
+
+
