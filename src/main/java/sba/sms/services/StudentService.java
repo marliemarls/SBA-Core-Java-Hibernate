a698603b207aedc0daf043b6ecc9d22b3204c832
Marlie Martinez <marlieahue@gmail.com>
2024-08-12 09:25:59 -0400

    finished

--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,18 +1,15 @@
 package sba.sms.services;
 
-import lombok.extern.java.Log;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
-import sba.sms.dao.CourseI;
 import sba.sms.dao.StudentI;
 import sba.sms.models.Course;
 import sba.sms.models.Student;
 import sba.sms.utils.HibernateUtil;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,31 +25,102 @@ public class StudentService implements StudentI {
 
     @Override
     public List<Student> getAllStudents() {
-        return List.of();
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        List<Student> studentList = new ArrayList<>();
+        try{
+            tx = s.beginTransaction();
+            Query<Student> q = s.createQuery("from Student", Student.class);
+            studentList = q.getResultList();
+            tx.commit();
+        } catch(HibernateException exception){
+            if(tx != null) tx.rollback();
+            exception.printStackTrace();
+        }finally {
+            s.close();
+        }
+        return studentList;
     }
 
     @Override
     public void createStudent(Student student) {
-
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try{
+            tx = s.beginTransaction();
+            s.persist(student);
+            tx.commit();
+        } catch(HibernateException exception){
+            if(tx != null) tx.rollback();
+            exception.printStackTrace();
+        }finally {
+            s.close();
+        }
     }
 
     @Override
     public Student getStudentByEmail(String email) {
-        return null;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        Student student = null;
+        try{
+            tx = s.beginTransaction();
+            Query<Student> q = s.createQuery("from Student where email = :email", Student.class);
+            q.setParameter("email", email);
+            student= q.getSingleResult();
+            tx.commit();
+        } catch(HibernateException exception){
+            if(tx != null) tx.rollback();
+            exception.printStackTrace();
+        }finally {
+            s.close();
+        }
+        return student;
     }
 
     @Override
     public boolean validateStudent(String email, String password) {
-        return false;
+        Student student = getStudentByEmail(email);
+        return student != null && student.getPassword().equals(password);
     }
 
     @Override
     public void registerStudentToCourse(String email, int courseId) {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
 
+        try{
+            tx = s.beginTransaction();
+            Student student = getStudentByEmail(email);
+            student.addCourse(courseService.getCourseById(courseId));
+            s.merge(student);
+            tx.commit();
+        } catch (HibernateException exception) {
+            if(tx != null) tx.rollback();
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
     }
 
     @Override
     public List<Course> getStudentCourses(String email) {
-        return List.of();
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        List<Course> courseList = new ArrayList<>();
+        try{
+            tx = s.beginTransaction();
+            String nativeGetStudentCourses = "select c.id, c.name, c.instructor from course as c join student_courses as sc on c.id = sc.courses_id join student as s on s.email = sc.student_email where s.email = :email";
+            NativeQuery<Course> studentCourses = s.createNativeQuery(nativeGetStudentCourses, Course.class);
+            studentCourses.setParameter("email", email);
+            courseList = studentCourses.getResultList();
+            tx.commit();
+        } catch(HibernateException exception){
+            if(tx != null) tx.rollback();
+            exception.printStackTrace();
+        }finally {
+            s.close();
+        }
+        return courseList;
     }
 }
